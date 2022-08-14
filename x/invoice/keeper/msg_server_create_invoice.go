7efb3c45e3ff7c71b6ef90b572d2f2f9e839053b
domsteil <domsteil14@gmail.com>
2022-08-14 16:26:22 -0700

    updating invoice module

--- x/invoice/keeper/msg_server_create_invoice.go
+++ x/invoice/keeper/msg_server_create_invoice.go
@@ -2,9 +2,13 @@ package keeper
 
 import (
 	"context"
+	"fmt"
 
+	
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	"github.com/stateset/core/x/invoice/types"
+
 )
 
 func (k msgServer) CreateInvoice(goCtx context.Context, msg *types.MsgCreateInvoice) (*types.MsgCreateInvoiceResponse, error) {
@@ -12,7 +16,6 @@ func (k msgServer) CreateInvoice(goCtx context.Context, msg *types.MsgCreateInvo
 
 	var invoice = types.Invoice {
 		Did:    msg.Did,
-		Uri:    msg.Uri,
 		Amount: msg.Amount,
 		State:  "requested",
 	}
--- x/invoice/keeper/msg_server_factor_invoice.go
+++ x/invoice/keeper/msg_server_factor_invoice.go
@@ -2,8 +2,10 @@ package keeper
 
 import (
 	"context"
+	"fmt"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	"github.com/stateset/core/x/invoice/types"
 )
 
