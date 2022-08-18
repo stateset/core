ae5ce045e67bda3b9dc3033e5bb92897c43e52d5
domsteil <domsteil14@gmail.com>
2022-08-18 13:15:30 -0700

    adding

--- x/invoice/types/expected_keepers.go
+++ x/invoice/types/expected_keepers.go
@@ -1,5 +1,10 @@
 package types
 
 type BankKeeper interface {
-	// Methods imported from bank should be defined here
-}
+	type BankKeeper interface {
+		// Methods imported from bank should be defined here
+		SendCoins(ctx sdk.Context, fromAddr sdk.AccAddress, toAddr sdk.AccAddress, amt sdk.Coins) error
+		SendCoinsFromAccountToModule(ctx sdk.Context, senderAddr sdk.AccAddress, recipientModule string, amt sdk.Coins) error
+		SendCoinsFromModuleToAccount(ctx sdk.Context, senderModule string, recipientAddr sdk.AccAddress, amt sdk.Coins) error
+	}
+	
