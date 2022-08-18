e9e29ca594a47d08d9d0a9ab5ea40c033c13a531
domsteil <domsteil14@gmail.com>
2022-08-18 12:14:09 -0700

    updating proto

--- vue/src/store/generated/stateset/core/stateset.core.purchaseorder/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { Purchaseorder } from "./module/types/purchaseorder/purchaseorder";
-import { SentPurchaseorder } from "./module/types/purchaseorder/sent_purchaseorder";
-import { TimedoutPurchaseorder } from "./module/types/purchaseorder/timedout_purchaseorder";
-export { Purchaseorder, SentPurchaseorder, TimedoutPurchaseorder };
-declare const _default;
-export default _default;
