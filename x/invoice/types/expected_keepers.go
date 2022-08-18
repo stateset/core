5063ff9f0132bd7a02c1fe1177b418a87e6c140c
domsteil <domsteil14@gmail.com>
2022-08-18 13:16:01 -0700

    adding

--- x/invoice/types/expected_keepers.go
+++ x/invoice/types/expected_keepers.go
@@ -7,4 +7,4 @@ type BankKeeper interface {
 		SendCoinsFromAccountToModule(ctx sdk.Context, senderAddr sdk.AccAddress, recipientModule string, amt sdk.Coins) error
 		SendCoinsFromModuleToAccount(ctx sdk.Context, senderModule string, recipientAddr sdk.AccAddress, amt sdk.Coins) error
 	}
-	
+}
