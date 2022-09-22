65529f7a4f8272a3daef862d12971fb30c0431d9
domsteil <domsteil14@gmail.com>
2022-09-22 13:00:22 -0700

    removing vue

--- vue/src/store/generated/index.d.ts
+++
@@ -1,8 +0,0 @@
-declare const _default: {
-    StatesetCoreStatesetCoreAgreement: (store: any) => void;
-    StatesetCoreStatesetCoreInvoice: (store: any) => void;
-    StatesetCoreStatesetCoreLoan: (store: any) => void;
-    StatesetCoreStatesetCorePurchaseorder: (store: any) => void;
-    StatesetCoreStatesetCoreRefund: (store: any) => void;
-};
-export default _default;
